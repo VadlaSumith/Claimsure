7bb2cd530493d4d46a4f199d9a9579580b7b9af9
Vadla Sumith <vsumith.17@gmail.com>
2025-07-21 23:32:10 -0400

    updated to H2 db for render deployment

--- src/main/java/com/example/claimsure_backend/ClaimsureBackendApplication.java
+++ src/main/java/com/example/claimsure_backend/ClaimsureBackendApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ClaimsureBackendApplication {
 
 	public static void main(String[] args) {
+		System.out.println(" Using H2 In-Memory Database");
 		SpringApplication.run(ClaimsureBackendApplication.class, args);
 	}
 
